ea0401c0e4afdbad9f8c6504c5175a216984d972
hkaur@ccsglobaltech.com <hkaur@ccsglobaltech.com>
2024-07-02 23:22:39 +0000

    update

---
+++ models/staging/stripe/stg_payment.sql
@@ -0,0 +1,13 @@
+with payment as (
+
+    select 
+    id as payment_id, 
+    orderid, 
+    paymentmethod, 
+    status, 
+    amount, 
+    created
+    from {{source('stripe','payment')}}
+
+)
+select * from payment
